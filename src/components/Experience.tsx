149fee9408d8af581f7864ccbac74ed8ca4f33eb
youssef1129 <maazouzyoussef11@gmail.com>
2022-09-06 16:51:16 +0100

    gdbls

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -12,9 +12,8 @@ const Experience: FC = () => {
         <section className='exp'>
 
             <div id='jobs'>
-                <button style={{ backgroundColor: job === 1 ? '#e4ffb01f' : '' }} onClick={() => setJob(1)}>{job === 1 && <IoMdArrowDropright />} plateforme nord afrique</button>
-                <button style={{ backgroundColor: job === 2 ? '#e4ffb01f' : '' }} onClick={() => setJob(2)}>{job === 2 && <IoMdArrowDropright />} plateforme nord afrique</button>
-                <button style={{ backgroundColor: job === 3 ? '#e4ffb01f' : '' }} onClick={() => setJob(3)}>{job === 3 && <IoMdArrowDropright />}plateforme nord afrique</button>
+                <button style={{ backgroundColor: job === 1 ? '#e4ffb01f' : '' }} onClick={() => setJob(1)}>{job === 1 && <IoMdArrowDropright />} Plateforme nord afrique</button>
+                <button style={{ backgroundColor: job === 2 ? '#e4ffb01f' : '' }} onClick={() => setJob(2)}>{job === 2 && <IoMdArrowDropright />} Label’Vie Group</button>
             </div>
 
             {
@@ -31,8 +30,9 @@ const Experience: FC = () => {
                 (<div>
                 <h3>Intership at plateforme nord afrique</h3>
                 <ul>
-                    <li><BiRadioCircleMarked/> desktop app development</li>
-                    <li><BiRadioCircleMarked/> database management</li>
+                    <li><BiRadioCircleMarked/> Mobile app development with react native</li>
+                    <li><BiRadioCircleMarked/> Web app development with angular </li>
+                    <li><BiRadioCircleMarked/> MongoDB </li>
                 </ul>
                 </div>)
                 :
