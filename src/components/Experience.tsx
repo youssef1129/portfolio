4199394fd10eed39fea219b2a3bac0b3fded85e3
youssef1129 <maazouzyoussef11@gmail.com>
2023-10-12 10:08:49 +0100

    fix

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,51 +2,75 @@ import React, { FC, useState } from 'react';
 import img from '../assets/reading.png'
 import { BiRadioCircleMarked } from 'react-icons/bi'
 import { IoMdArrowDropright } from 'react-icons/io'
+const jobs = [
+    {
+        company: 'Algo Consulting Group',
+        title: 'FullStack developer at algo consulting group',
+        stack: [
+            'Preparing the models.',
+            'Use of technologies such as ReactJS and Express.js for the creation of the user interface and the backend.',
+            'Implemented advanced features to ensure a smooth and user-friendly',
+        ],
+    },
+    {
+        company: 'Label’Vie Group',
+        title: 'Middleware administrator at Label’Vie Group',
+        stack: [
+            "frontend reactjs nextjs",
+            "backend nestjs",
+            "databases postgreSql Mongodb"
+        ]
+    },
+    {
+        company: 'Label’Vie Group',
+        title: 'Fullstack developer Intership at Label’Vie Group',
+        stack: [
+            'Mobile App Development With React Native',
+            ' Web App Development With Angular',
+            'MongoDB',
+        ],
+    },
+    {
+        company: 'Plateforme nord afrique',
+        title: 'Intership at plateforme nord afrique',
+        stack: [
+            'desktop app development',
+            'database management',
+            'ui designing',
+        ],
+    }
+]
 
 const Experience: FC = () => {
 
-    const [job, setJob] = useState<number>(1)
+    const [currentJob, setCurrentJob] = useState(jobs[0])
 
     return <div className='box' data-aos="zoom-in" id='exp'>
 
         <section className='exp'>
-
             <div id='jobs'>
-                <button style={{ backgroundColor: job === 1 ? '#e4ffb01f' : '' }} onClick={() => setJob(1)}>{job === 1 && <IoMdArrowDropright />} Label’Vie Group</button>
-                <button style={{ backgroundColor: job === 2 ? '#e4ffb01f' : '' }} onClick={() => setJob(2)}>{job === 2 && <IoMdArrowDropright />} Label’Vie Group</button>
-                <button style={{ backgroundColor: job === 3 ? '#e4ffb01f' : '' }} onClick={() => setJob(3)}>{job === 3 && <IoMdArrowDropright />} Plateforme nord afrique</button>
+                {
+                    jobs.map((j) => {
+                        return <button
+                            style={{ backgroundColor: currentJob.title === j.title ? '#e4ffb01f' : '' }}
+                            onClick={() => setCurrentJob(jobs[jobs.findIndex((t) => t.title === j.title)])}>{j.title === currentJob.title && <IoMdArrowDropright />} {j.company}</button>
+                    })
+                }
             </div>
 
-            {
-                job === 1 ?
-                    (<div>
-                        <h3>Middleware administrator at Label’Vie Group</h3>
-                        <ul>
-                            <li><BiRadioCircleMarked /> frontend reactjs nextjs</li>
-                            <li><BiRadioCircleMarked /> backend nestjs</li>
-                            <li><BiRadioCircleMarked /> databases postgreSql Mongodb</li>
-                        </ul>
-                    </div>)
-                    : job === 2 ?
-                        (<div>
-                            <h3>Fullstack developer Intership at Label’Vie Group</h3>
-                            <ul>
-                                <li><BiRadioCircleMarked /> Mobile App Development With React Native</li>
-                                <li><BiRadioCircleMarked /> Web App Development With Angular</li>
-                                <li><BiRadioCircleMarked /> MongoDB</li>
-                            </ul>
-                        </div>)
-                        :
-                        (<div>
-                            <h3>Intership at plateforme nord afrique</h3>
-                            <ul>
-                                <li><BiRadioCircleMarked /> desktop app development</li>
-                                <li><BiRadioCircleMarked /> database management</li>
-                                <li><BiRadioCircleMarked /> ui designing</li>
-                                <li><BiRadioCircleMarked /> ui designing</li>
-                            </ul>
-                        </div>)
-            }
+
+            <div>
+                <h3>{currentJob.title}</h3>
+                <ul>
+                    {
+                        currentJob.stack.map((s) => {
+                            return <li><BiRadioCircleMarked /> {s}</li>
+                        })
+                    }
+                </ul>
+            </div>
+
+
         </section>
 
         <img src={img} alt='' />
