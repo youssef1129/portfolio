217b219f40f567ca3f74fe992856f0bf409fafc9
youssef1129 <maazouzyoussef11@gmail.com>
2023-12-22 11:16:16 +0100

    f

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,13 +8,13 @@ const About = () => {
   return <div className='box' data-aos="fade-right" id='about'>
       <img src={img} alt='' />
       <section>
-          <code>hello my name is</code>
-          <h1>youssef maazouz.</h1>
-          <h3>welcome to my portfolio.</h3>
-          <p>I'm a software developper currently working
+          <code>hello, my name is</code>
+          <h1>Yyoussef Maazouz.</h1>
+          <h3>Welcome to my portfolio.</h3>
+          <p>I'm a software developer currently working
              at Algo Consulting Group , i'm specialized in building web/mobile
              applications with multiple stacks and tools.<br/><br/>
-             Here are a few technologies I’ve been working with recently:<br/><br/>
+             Here are a few technologies I have been working with recently:<br/><br/>
              <FaReact/> <SiTypescript/> <IoLogoJavascript/> <FaNodeJs/> <SiFirebase/>
           </p>
       </section>
